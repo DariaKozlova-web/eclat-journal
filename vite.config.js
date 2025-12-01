af163b4419a9df124b263af80b2c7fcacc0316d4
Daria Kozlova <kozloffa.daria@gmail.com>
2025-12-01 22:36:58 +0100

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import process from 'node:process'
 
-// https://vite.dev/config/
+// Detect if we are building for GitHub Pages
+const isGitHubPages = process.env.HOSTING_ON_GHP === 'true'
+
 export default defineConfig({
-  base: '/eclat-journal/',
+  base: isGitHubPages ? '/eclat-journal/' : '/',
   plugins: [react(), tailwindcss()],
 })
